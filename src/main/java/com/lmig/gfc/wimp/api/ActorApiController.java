f11b8ff2b77e31629a5df1770109a72e7ff0b183
Rob Johnson <rob.johnson@libertymutual.com>
2018-01-03 11:15:31 -0500

    added access logic to controllers

--- src/main/java/com/lmig/gfc/wimp/api/ActorApiController.java
+++ src/main/java/com/lmig/gfc/wimp/api/ActorApiController.java
@@ -3,6 +3,7 @@ package com.lmig.gfc.wimp.api;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.lmig.gfc.wimp.models.Actor;
 import com.lmig.gfc.wimp.repositories.ActorRepository;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/actors")
 public class ActorApiController {
--- src/main/java/com/lmig/gfc/wimp/api/MovieApiController.java
+++ src/main/java/com/lmig/gfc/wimp/api/MovieApiController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.lmig.gfc.wimp.models.Movie;
 import com.lmig.gfc.wimp.repositories.MovieRepository;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/movies")
 public class MovieApiController {
